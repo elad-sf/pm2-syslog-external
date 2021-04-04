8cc820cda9235401ecdb047fa38e94b67389d960
Elad Mizrahi <elad@silverfort.com>
2021-04-04 13:48:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const pm2 = require('pm2');
+const SysLogger = require('ain2');
+const logger = new SysLogger({ tag: 'pm2', facility: 'syslog', address: 'syslog' });
+
+pm2.launchBus(function(err, bus) {
+    bus.on('*', function(event, data) {
+        if (event === 'process:event') {
+            logger.warn(
+                'app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
+                data.process.name,
+                data.process.pm_id,
+                data.process.restart_time,
+                data.event
+            );
+        }
+    });
+
+    bus.on('log:err', function(data) {
+        logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    });
+
+    bus.on('log:out', function(data) {
+        logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    });
+});
